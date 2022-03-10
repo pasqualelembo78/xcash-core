616450e504dc7cd45e687c95129212c527f892e6
zachhildreth <zachariahildreth@gmail.com>
2022-03-10 13:48:14 -0800

    fix: double light hard fork
    double light hard fork

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -254,7 +254,7 @@ full_addrs.insert(SEED_NODE_5);
 
 
 // Updates data
-#define BLOCK_HEIGHT_SF_V_2_1_0 900030 // stealth address verification
+#define BLOCK_HEIGHT_SF_V_2_1_0 900050 // stealth address verification
 
 
 // Network data nodes
