17ebeacecdc20b0ad2a2c047c4e129c62f58f9b0
zachhildreth <zachariahildreth@gmail.com>
2022-03-14 17:47:26 -0700

    fix: test rd block height
    test rd block height

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -259,7 +259,7 @@ full_addrs.insert(SEED_NODE_5);
 #define BLOCK_HEIGHT_SF_V_2_2_0 900100 // stealth address verification
 
 // Remote Data
-#define HF_BLOCK_HEIGHT_REMOTE_DATA 8000000 // The first block of the remote data
+#define HF_BLOCK_HEIGHT_REMOTE_DATA 900885 // The first block of the remote data
 
 
 // Network data nodes
